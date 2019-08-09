26ceabd3515a5b687de6c1e13961335dbfd4ddf0
antaril <pepsticker@gmx.de>
2019-08-09 14:31:29 -0400

    emby4: update to 4.2.0.40

--- packages/addons/service/emby4/package.mk
+++ packages/addons/service/emby4/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="emby4"
-PKG_VERSION="4.1.0.26"
-PKG_SHA256="ccb9bb463e6ff5e084f731ccbb48119176cdc3592b3234796eefa09ec9e03e0f"
-PKG_REV="102"
+PKG_VERSION="4.2.0.40"
+PKG_SHA256="2c016f5dea90b30beecc06753980deab764ece5ff6d6ef20e7108dca137979dd"
+PKG_REV="104"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
 PKG_SITE="http://emby.media"
